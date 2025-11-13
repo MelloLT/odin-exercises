5d28ba11344f745424639731d80bf97053840ee0
MelloLT <nurmamedova28@gmail.com>
2025-11-13 18:09:37 +0300

    first commit

---
+++ javascript-exercises/03_numberChecker/numberChecker.js
@@ -0,0 +1,10 @@
+function numberChecker(number) {
+  if (number === 10 || number === 1000) {
+    return true;
+  } else if (number === 9 || number === 6) {
+    return false;
+  }
+}
+
+// Do not edit below this line
+module.exports = numberChecker;
